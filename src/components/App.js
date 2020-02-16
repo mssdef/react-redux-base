dc935d75a900f3f7aa122f37c872b541510000a5
Andrew <andrewrr@Andrejs-MacBook-Air.local>
2020-02-16 16:00:41 +0200

    upd

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//import { selectSong } from '../actions';
+
+const App = () => {
+  return <div className="app-block"><h1>redux app</h1></div>;
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+const songReducer = () => {
+  return [
+    { title: 'personal jesus', duration: '4:05'},
+    { title: 'hit it up', duration: '3:17'},
+    { title: 'something to say', duration: '3:58'},
+    { title: 'fade to black', duration: '5:30'}
+
+  ]
+};
+
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type == 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
