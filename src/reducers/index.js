5ecb6a3f8e9cc3ee3c4cd7eb9b13207e2fd29942
Andrew <dev@xpscommerce.com>
2025-07-19 19:02:06 +0200

    pr-2025-07-19 (#22)
    * improvement
    
    * improvement
    
    * improvement
    
    ---------
    
    Co-authored-by: Andreu Romanc <andreur@Mac16.local>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,6 @@
 import { combineReducers } from 'redux';
 import { SONG_SELECTED } from '../actions/types';
-
-const SONGS_DATA = [
-  { title: 'No Scrubs', duration: '4:05' },
-  { title: 'Macarena', duration: '2:30' },
-  { title: 'All Star', duration: '3:15' },
-  { title: 'I Want it That Way', duration: '1:45' }
-];
+import { SONGS_DATA } from '../data/songs';
 
 const songsReducer = () => {
   return SONGS_DATA;
