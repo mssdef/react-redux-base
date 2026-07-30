ac4726b30cda446afe128a66e4f2472bce844133
Andrew <dev@xpscommerce.com>
2026-07-30 23:15:14 +0200

    update

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { nextSong } from '../actions';
+import { SONGS_DATA } from '../data/songs';
 import { selectSong } from '../actions';
 
 const SongList = () => {
   const songs = useSelector(state => state.songs);
   const dispatch = useDispatch();
+  const [currentSongIndex, setCurrentSongIndex] = React.useState(0);
+
+  const nextSong = () => {
+    setCurrentSongIndex(prevIndex => (prevIndex + 1) % songs.length);
+  };
 
   const renderList = () => {
-    return songs.map(song => {
+    return songs.map((song, index) => {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
@@ -17,6 +24,14 @@ const SongList = () => {
             >
               Select
             </button>
+            {index === currentSongIndex && (
+              <button
+                className="ui button"
+                onClick={nextSong}
+              >
+                Next
+              </button>
+            )}
           </div>
           <div className="content">{song.title}</div>
         </div>
